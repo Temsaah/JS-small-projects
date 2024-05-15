93b508067628ad99e1c5d7265276527014e0ee2c
Temsaah <Seif.Temsaah@gmail.com>
2024-05-15 17:18:50 +0300

    Added JS Part

--- FAQ/app.js
+++ FAQ/app.js
@@ -1,12 +1,16 @@
-const modalBtn = document.querySelector(".modal-btn");
-const modalContainer = document.querySelector(".modal-container");
+const plusIcons = document.querySelectorAll(".plus-icon");
+const minusIcons = document.querySelectorAll(".minus-icon");
+const questions = document.querySelectorAll(".question");
 
-modalBtn.addEventListener("click", () => {
-  modalContainer.classList.toggle("hidden");
-});
-
-modalContainer.addEventListener("click", () => {
-  if (!modalContainer.classList.contains("hidden")) {
-    modalContainer.classList.add("hidden");
-  }
+questions.forEach((question) => {
+  question.addEventListener("click", (e) => {
+    if (
+      e.target.parentElement.classList.contains("plus-icon") ||
+      e.target.parentElement.classList.contains("minus-icon")
+    ) {
+      question.querySelector(".plus-icon").classList.toggle("hidden");
+      question.querySelector(".minus-icon").classList.toggle("hidden");
+      question.querySelector(".answer").classList.toggle("hidden");
+    }
+  });
 });
