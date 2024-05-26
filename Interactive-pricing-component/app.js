71979a14a8f94613a312d1f38dfdb847668b91ba
Temsaah <Seif.Temsaah@gmail.com>
2024-05-26 18:16:34 +0300

    Added interactivity to toggle switch button + discount on yearly plan

--- Interactive-pricing-component/app.js
+++ Interactive-pricing-component/app.js
@@ -6,10 +6,14 @@ const price = document.querySelector(".price-label");
 const toggleSwitch = document.querySelector(".toggle-switch");
 const switchCircle = document.querySelector(".switch-circle");
 
-let monthlyPlan = true;
+let plan = "monthly";
+
+let discount = 25;
+
+let val = 50;
 
 sliderBar.addEventListener("input", () => {
-  let val = sliderBar.value;
+  val = sliderBar.value;
   console.log(val);
   sliderBar.style.backgroundImage = `linear-gradient(
     90deg,
@@ -25,10 +29,20 @@ function updatePageViews(val) {
 }
 
 function updatePrice(val) {
-  price.textContent = `$${((16 / 50) * val).toFixed(2)}`;
+  if (plan == "monthly") {
+    price.textContent = `$${((16 / 50) * val).toFixed(2)}`;
+  } else if (plan == "yearly") {
+    let yearlyPrice = (16 / 50) * val * 12;
+    let discountedPrice = yearlyPrice - yearlyPrice * (discount / 100);
+    price.textContent = `$${discountedPrice.toFixed(2)}`;
+  }
 }
 
 toggleSwitch.addEventListener("click", () => {
   toggleSwitch.classList.toggle("switch-on");
   switchCircle.classList.toggle("switch-on");
+  if (plan == "monthly") plan = "yearly";
+  else plan = "monthly";
+
+  updatePrice(val);
 });
