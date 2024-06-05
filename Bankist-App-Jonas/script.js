8d7d85e06d416bc8e450057abeb0c76569bc06eb
Temsaah <Seif.Temsaah@gmail.com>
2024-06-05 22:14:53 +0300

    Show time of each transaction made

--- Bankist-App-Jonas/script.js
+++ Bankist-App-Jonas/script.js
@@ -7,9 +7,22 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2024-07-26T23:36:17.929Z',
+    '2024-07-20T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,6 +30,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -24,6 +50,18 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account4 = {
@@ -31,6 +69,18 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -77,6 +127,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 let currentAccount;
 
+// Fake Login
+
+currentAccount = account1;
+updateMovementContainer(currentAccount);
+containerApp.style.opacity = '100';
+
 const createUserNames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -109,13 +165,14 @@ btnLogin.addEventListener('click', e => {
 
 btnLoan.addEventListener('click', e => {
   e.preventDefault();
-  const loanAmount = Number(inputLoanAmount.value);
+  const loanAmount = Number(Number(inputLoanAmount.value).toFixed(2));
 
   if (
     loanAmount > 0 &&
     currentAccount.movements.some(mov => mov > loanAmount * (10 / 100))
   ) {
     currentAccount.movements.push(loanAmount);
+    currentAccount.movementsDates.push(new Date().toISOString());
     updateMovementContainer(currentAccount);
   }
   inputLoanAmount.value = '';
@@ -134,7 +191,9 @@ btnTransfer.addEventListener('click', e => {
     receiverAcc.username !== currentAccount.username
   ) {
     currentAccount.movements.push(-Number(inputTransferAmount.value));
+    currentAccount.movementsDates.push(new Date().toISOString());
     receiverAcc.movements.push(+inputTransferAmount.value);
+    receiverAcc.movementsDates.push(new Date().toISOString());
     updateMovementContainer(currentAccount);
   }
   inputTransferTo.value = inputTransferAmount.value = '';
@@ -172,11 +231,29 @@ function findAccountByUserName(userName) {
 }
 
 function getBalance(account) {
-  return account.movements.reduce((acc, curr) => acc + curr);
+  return account.movements.reduce((acc, curr) => acc + curr).toFixed(2);
+}
+
+function formatMovementDate(date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth()}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 }
 
 function updateMovementContainer(account, sort = false) {
   containerMovements.innerHTML = '';
+  const now = new Date();
 
   const movs = sort
     ? account.movements.slice().sort((a, b) => a - b)
@@ -185,6 +262,9 @@ function updateMovementContainer(account, sort = false) {
   movs.forEach((movement, i) => {
     let movementType;
 
+    const date = new Date(account.movementsDates[i]);
+    const displayDate = formatMovementDate(date);
+
     if (movement > 0) movementType = 'deposit';
     else movementType = 'withdrawal';
 
@@ -192,27 +272,34 @@ function updateMovementContainer(account, sort = false) {
         <div class="movements__type movements__type--${movementType}">${
       i + 1
     } ${movementType}</div>
-        <div class="movements__value">${movement}€</div>
+        <div class="movements__date">${displayDate}</div>
+        <div class="movements__value">${movement.toFixed(2)}€</div>
       </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', element);
 
     labelBalance.textContent = `${getBalance(account)}€`;
+    labelDate.textContent = `${String(now.getDate()).padStart(2, 0)}/${String(
+      now.getMonth() + 1
+    ).padStart(2, 0)}/${now.getFullYear()}, ${String(now.getHours()).padStart(
+      2,
+      0
+    )}:${String(now.getMinutes()).padStart(2, 0)}`;
   });
 
   const sumIn = account.movements
     .filter(mov => mov > 0)
     .reduce((acc, curr) => acc + curr, 0);
-  labelSumIn.textContent = `${sumIn}€`;
+  labelSumIn.textContent = `${sumIn.toFixed(2)}€`;
 
   const sumOut = account.movements
     .filter(mov => mov < 0)
     .reduce((acc, curr) => acc + curr, 0);
-  labelSumOut.textContent = `${Math.abs(sumOut)}€`;
+  labelSumOut.textContent = `${Math.abs(sumOut.toFixed(2))}€`;
 
   const interest = account.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * account.interestRate) / 100)
     .filter(int => int >= 1)
     .reduce((acc, curr) => acc + curr, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 }
