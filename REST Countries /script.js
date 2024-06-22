3e67ebc191c73e338d6aa9b73ac6306ed001d08f
Temsaah <Seif.Temsaah@gmail.com>
2024-06-22 13:14:12 +0300

    Fix Callback hell by using fetch api

--- REST Countries /script.js
+++ REST Countries /script.js
@@ -5,67 +5,67 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryAndNeighbourData = function (country) {
-  const request = new XMLHttpRequest();
+// const getCountryAndNeighbourData = function (country) {
+//   const request = new XMLHttpRequest();
 
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
-  request.send();
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const data = JSON.parse(this.responseText);
-    console.log(data);
+//   request.addEventListener('load', function () {
+//     const data = JSON.parse(this.responseText);
+//     console.log(data);
 
-    renderCountry(data);
+//     renderCountry(data);
 
-    const [neighbour] = data[0]?.borders;
+//     const [neighbour] = data[0]?.borders;
 
-    if (!neighbour) return;
+//     if (!neighbour) return;
 
-    const request2 = new XMLHttpRequest();
+//     const request2 = new XMLHttpRequest();
 
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
 
-    request2.send();
+//     request2.send();
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      renderCountry(data2);
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       renderCountry(data2);
 
-      const [neighbour] = data2[0]?.borders;
+//       const [neighbour] = data2[0]?.borders;
 
-      if (!neighbour) return;
+//       if (!neighbour) return;
 
-      const request3 = new XMLHttpRequest();
+//       const request3 = new XMLHttpRequest();
 
-      request3.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//       request3.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
 
-      request3.send();
+//       request3.send();
 
-      request3.addEventListener('load', function () {
-        const data3 = JSON.parse(this.responseText);
-        console.log(data3);
-        renderCountry(data3);
-      });
-    });
-  });
-};
+//       request3.addEventListener('load', function () {
+//         const data3 = JSON.parse(this.responseText);
+//         console.log(data3);
+//         renderCountry(data3);
+//       });
+//     });
+//   });
+// };
 
-function renderCountry(data) {
+function renderCountry(data, className = '') {
   const html = `
-  <article class="country">
-        <img class="country__img" src="${data[0].flags.png}" />
+  <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" />
         <div class="country__data">
-          <h3 class="country__name">${data[0].name.common}</h3>
-          <h4 class="country__region">${data[0].region}</h4>
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
           <p class="country__row"><span>👫</span>${(
-            +data[0].population / 1000000
-          ).toFixed(1)}</p>
+            +data.population / 1000000
+          ).toFixed(1)}m people</p>
           <p class="country__row"><span>🗣️</span>${
-            Object.values(data[0].languages)[0]
+            Object.values(data.languages)[0]
           }</p>
           <p class="country__row"><span>💰</span>${
-            Object.values(data[0].currencies)[0].name
+            Object.values(data.currencies)[0].name
           }</p>
         </div>
   </article>`;
@@ -73,4 +73,20 @@ function renderCountry(data) {
   countriesContainer.insertAdjacentHTML('beforeend', html);
 }
 
-getCountryAndNeighbourData('Germany');
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(res => res.json())
+    .then(function (data) {
+      renderCountry(data[0]);
+
+      const neighbour = data[0]?.borders[0];
+
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(res => res.json())
+    .then(neighbour => renderCountry(neighbour[0], 'neighbour'));
+};
+
+getCountryData('Germany');
